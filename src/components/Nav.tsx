434015b96b25f639c6d4aa4e41505858861efb9d
sevenliao <liaoyuwang@126.com>
2025-06-23 21:55:28 +0800

    temp: update nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 const NAVMAIM = function() {
-    return <div>
-        <h1 className="text-3xl font-bold text-center text-gray-900 dark:text-white mb-8">sevenliao站点</h1>
-    </div>
+    return(
+        <div>
+            <h1 className="text-3xl font-bold text-center text-gray-900 dark:text-white mb-8">sevenliao站点</h1>
+        </div>
+    )
 }
 
 export default NAVMAIM
